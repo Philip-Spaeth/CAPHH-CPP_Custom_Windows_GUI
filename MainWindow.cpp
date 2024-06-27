2b064332d8624f2a5787d57b48dbaa898b94a50e
Philip Spaeth <philip.spaeth@outlook.com>
2024-06-27 22:19:23 +0200

    new text

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -7,6 +7,11 @@
 
 using namespace std;
 
+void newBranch()
+{
+	cout << "new branch" << endl;
+}
+
 void MainWindow::Print()
 {
     AddElement(std::make_shared<TextBlock>(text, 400, 50, 30, 10));
@@ -16,7 +21,7 @@ MainWindow::MainWindow(HINSTANCE hInstance) : Window(hInstance)
 {
     //buttons
     AddElement(std::make_shared<Button>(L"Button 1", 10, 10, 100, 30, [this]() { Print(); }));
-    AddElement(std::make_shared<Button>(L"der andere button", 37, 490, 130, 90, [this]() { Print(); }));
+    AddElement(std::make_shared<Button>(L"ere button", 37, 490, 130, 90, [this]() { Print(); }));
 
     //Textblocks
     AddElement(std::make_shared<TextBlock>(L"der dass hier geschrieben hat muss sehr gut sein", 10, 50, 5, 10));
