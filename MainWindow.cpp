fc721e3fd566302c52eaa4eac8994a89f2ce4843
Philip Spaeth <philip.spaeth@outlook.com>
2024-06-27 22:13:40 +0200

    new changes

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -15,8 +15,8 @@ void MainWindow::Print()
 MainWindow::MainWindow(HINSTANCE hInstance) : Window(hInstance)
 {
     //buttons
-    AddElement(std::make_shared<Button>(L"Klicken", 10, 10, 100, 30, [this]() { Print(); }));
-    AddElement(std::make_shared<Button>(L"Klwrken", 37, 490, 130, 90, [this]() { Print(); }));
+    AddElement(std::make_shared<Button>(L"Button 1", 10, 10, 100, 30, [this]() { Print(); }));
+    AddElement(std::make_shared<Button>(L"der andere button", 37, 490, 130, 90, [this]() { Print(); }));
 
     //Textblocks
     AddElement(std::make_shared<TextBlock>(L"Hal der sie geschrieben hat muss sehr gut sein", 10, 50, 5, 10));
